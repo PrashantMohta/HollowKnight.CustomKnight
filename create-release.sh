89407f16299a0006f2ce3d108e15af7420ac6dd7
Prashant Mohta <prashant.mohta@testbook.com>
2021-06-07 01:44:19 +0530

    update release script

--- create-release.sh
+++ create-release.sh
@@ -2,9 +2,9 @@ cd CustomKnight
 msbuild -p:Configuration=Release
 
 cp -R -X ../Releases/Template/ ../Releases/Temp/
-mv -f ./bin/Release/CustomKnight.dll ../Releases/Temp/hollow_knight_Data/Managed/Mods/CustomKnight/CustomKnight.dll
-cd ../Releases
-zip -r ./CustomKnight.zip ./Temp/.  -x "**/.DS_Store" -x "**/__MACOSX"
-cd ..
-
+mv -f ./bin/Release/CustomKnight.dll ../Releases/Temp/hollow_knight_Data/Managed/Mods/CustomKnight.dll
+cd ../Releases/Temp/
+zip -r ../CustomKnight.zip .  -x "**/.DS_Store" -x "**/__MACOSX"
+cd ../../
+openssl sha1 ./Releases/Temp/hollow_knight_Data/Managed/Mods/CustomKnight.dll
 
