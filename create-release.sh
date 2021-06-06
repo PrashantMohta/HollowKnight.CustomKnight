7f21f8d7a5c3120bd6c871bd364bdc70e664bd8e
Prashant Mohta <prashant.mohta@testbook.com>
2021-06-06 16:09:53 +0530

    update scripts

--- create-release.sh
+++ create-release.sh
@@ -5,5 +5,6 @@ cp -R -X ../Releases/Template/ ../Releases/Temp/
 mv -f ./bin/Release/CustomKnight.dll ../Releases/Temp/hollow_knight_Data/Managed/Mods/CustomKnight/CustomKnight.dll
 cd ../Releases
 zip -r ./CustomKnight.zip ./Temp/.  -x "**/.DS_Store" -x "**/__MACOSX"
+cd ..
 
 
