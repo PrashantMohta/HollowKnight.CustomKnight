33151749f76f78a047e8608550109c9e478aab3b
Prashant Mohta <prashant.mohta@testbook.com>
2021-06-08 16:26:56 +0530

    only the cleanest zips for our release

--- create-release.sh
+++ create-release.sh
@@ -4,7 +4,8 @@ msbuild -p:Configuration=Release
 cp -R -X ../Releases/Template/ ../Releases/Temp/
 mv -f ./bin/Release/CustomKnight.dll ../Releases/Temp/hollow_knight_Data/Managed/Mods/CustomKnight.dll
 cd ../Releases/Temp/
-zip -r ../CustomKnight.zip .  -x "**/.DS_Store" -x "**/__MACOSX"
+zip -r ../CustomKnight.zip .  -x ".*"  -x "**/.DS_Store" -x "**/__MACOSX"
 cd ../../
+unzip -l ./Releases/CustomKnight.zip
 openssl sha1 ./Releases/Temp/hollow_knight_Data/Managed/Mods/CustomKnight.dll
 
