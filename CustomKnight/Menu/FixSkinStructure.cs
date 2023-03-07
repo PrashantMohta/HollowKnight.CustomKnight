573b734b8ac26abcd11f8f9d0d0f2dd6c3ca6f28
Prashant Mohta <mohtaprashantrocks@gmail.com>
2023-03-07 16:19:27 +0530

    fix skin button aware for swap and cinematics now

--- CustomKnight/Menu/FixSkinStructure.cs
+++ CustomKnight/Menu/FixSkinStructure.cs
@@ -3,7 +3,7 @@ namespace CustomKnight
 {
     internal static class FixSkinStructure
     {
-
+        
         internal static bool dirHasPng(string sourceDirectory, SearchOption op){
            return IoUtils.DirectoryHas(sourceDirectory, "*.png", op);
         }
@@ -13,8 +13,18 @@ namespace CustomKnight
                 List<string> queue = new List<string>();
                 string[] dirs = Directory.GetDirectories(currentPath);
                 foreach (string dir in dirs){
-                    CustomKnight.Instance.Log("Looking in" + dir);
-                    if(dirHasPng(dir,SearchOption.TopDirectoryOnly)){
+                    CustomKnight.Instance.Log("Looking in" + dir + "|" + Path.GetDirectoryName(dir));
+
+                    if (Path.GetFileName(dir) == "Swap")
+                    {
+                        CustomKnight.Instance.Log("Found Swap Directory");
+                        IoUtils.DirectoryCopyAllFiles(dir, Path.Combine(root, "Swap"), true, true);
+                    }
+                    else if(Path.GetFileName(dir) == "Cinematics")
+                    {
+                        CustomKnight.Instance.Log("Found Cinematics Directory");
+                        IoUtils.DirectoryCopyAllFiles(dir, Path.Combine(root, "Cinematics"), true, true);
+                    } else if (dirHasPng(dir,SearchOption.TopDirectoryOnly)){
                         IoUtils.DirectoryCopyAllFiles(dir,root);
                     } else if(dirHasPng(dir,SearchOption.AllDirectories)){
                         queue.Add(dir);
